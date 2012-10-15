530f8c44d7aca046250051acfb78605adbd88d2f
hbc <bcxxxxxx@gmail.com>
2012-10-15 21:16:14 +0800

    fixed typo

--- server/translator/__init__.py
+++ server/translator/__init__.py
@@ -1,7 +1,7 @@
 #coding: utf-8
 
 from .human2machine import human2machine
-from .machine2human import manchine2human
+from .machine2human import machine2human
 
 
-__all__ = [human2machine, manchine2human]
+__all__ = [human2machine, machine2human]
